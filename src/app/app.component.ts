c6e4d59f68da17b386a31ac0d9aa8b26d0f963be
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-10-03 22:57:50 -0300

    posición actual en mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 
 declare var H: any;
 
@@ -10,6 +10,16 @@ declare var H: any;
 export class AppComponent implements OnInit {
 
     private platform: any;
+    public latitud: any;
+    public longitud: any;
+    @Input()
+    public appId: any;
+
+    @Input()
+    public appCode: any;
+
+    public map: any;
+    public marker: any;
 
     @ViewChild("map")
     public mapElement: ElementRef;
@@ -21,18 +31,46 @@ export class AppComponent implements OnInit {
         });
     }
 
-    public ngOnInit() { }
+    public ngOnInit() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((res) => {
+                this.latitud = res.coords.latitude;
+                this.longitud = res.coords.longitude;
+                let latitud = this.latitud;
+                let longitud = this.longitud;
+                this.centerMap(latitud, longitud);
+            })
+        }
+     }
 
-    public ngAfterViewInit() {
-        let defaultLayers = this.platform.createDefaultLayers();
+     public centerMap(latitud,longitud) {
+        let platform = new H.service.Platform({
+            "app_id": "KbKafnCBtngVMwl6F2zQ",
+            "app_code": "-K3tSTmhfjA5JPOhfAKU9w"
+        });
+        let defaultLayers = platform.createDefaultLayers();
         let map = new H.Map(
             this.mapElement.nativeElement,
             defaultLayers.normal.map,
             {
-                zoom: 10,
-                center: { lat: 37.7397, lng: -121.4252 }
+                zoom: 18,
+                center: { lat: latitud, lng: longitud }
             }
         );
+        this.markerCurrentPosition(latitud, longitud);
+        let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
+    }
+
+    markerCurrentPosition(latitud, longitud){
+        let icon = new H.map.Icon('../assets/img/marker.jpg'),
+          coords = {
+            lat: latitud,
+            lng: longitud
+          },
+          marker = new H.map.Marker(coords, {
+            icon: icon
+          });
+        this.map.addObject(marker);
     }
 
 }
